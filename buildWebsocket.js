51a24acb5fb7d91ceb2e95945736d8fb801d7b38
Matteo Collina <matteo.collina@gmail.com>
2014-01-29 00:37:58 +0100

    Working now.

--- buildWebsocket.js
+++ buildWebsocket.js
@@ -3,6 +3,6 @@ var websocket = require('websocket-stream')
 
 module.exports = function buildWebsocket(url, protocol, opts) {
   opts = opts || {};
-  opts.protocol = 'mqttv3.1';
+  opts.protocol = { protocol: 'mqttv3.1' };
   return websocket(url, opts);
 };
